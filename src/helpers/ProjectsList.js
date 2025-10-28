e231b4635c447f4b1fb03ea1c81f30719a3fd976
Asel <aabdykalykova186@gmail.com>
2025-10-28 09:11:53 +0100

    add projects

--- src/helpers/ProjectsList.js
+++ src/helpers/ProjectsList.js
@@ -1,35 +1,43 @@
-import project01 from "./../img/projects/neoTour.png";
-import project02 from "./../img/projects/the-meal.png";
-import project03 from "./../img/projects/gyphy.png";
-import project04 from "./../img/projects/movie.png";
+import project01 from './../img/projects/beauty.png';
+import project02 from './../img/projects/the-meal.png';
+import project03 from './../img/projects/gyphy.png';
+import project04 from './../img/projects/movie.png';
+import project05 from './../img/projects/pokemon.png';
 const projects = [
-    {
-        title: "neoTour",
-        img: project01,
-        skills:"JavaScript, ReactJS,HTML, CSS, NPM,API",
-        href:"https://neo-tour-lovat.vercel.app/",
-        gitHub:"https://github.com/Asel85/neobis-neo-tour"
-    },
-    {
-        title: "The Meal",
-        img: project02,
-        skills:"JavaScript, ReactJS,HTML, CSS, NPM,API",
-        href:"https://my-repository-five-tawny.vercel.app/",
-        gitHub:"https://github.com/Asel85/neobis-front-themeal"
-    },
-    {
-        title: "gyphyAPI",
-        img: project03,
-        skills:"JavaScript,HTML, CSS, NPM, API",
-        href:"https://giphy-gules.vercel.app/",
-        gitHub:"https://github.com/Asel85/neobis-front-giphyAPI"
-    },
-    {
-        title: "Movie App",
-        img: project04,
-        skills:"JavaScript,HTML, CSS, NPM, API",
-        href:"https://cinema-five-phi.vercel.app/",
-        gitHub:"https://github.com/Asel85/neobis-front-cinema"
-    }
-]
-export {projects}
+  {
+    title: 'beauty-web',
+    img: project01,
+    skills: 'JavaScript, ReactJS,HTML, CSS, Scss, Bootstrap',
+    href: 'https://beauty-web3.vercel.app/',
+    gitHub: 'https://github.com/Asel85/beauty-web3',
+  },
+  {
+    title: 'The Meal',
+    img: project02,
+    skills: 'JavaScript, ReactJS,HTML, CSS, NPM,API',
+    href: 'https://my-repository-five-tawny.vercel.app/',
+    gitHub: 'https://github.com/Asel85/neobis-front-themeal',
+  },
+  {
+    title: 'gyphyAPI',
+    img: project03,
+    skills: 'JavaScript,HTML, CSS, NPM, API',
+    href: 'https://giphy-gules.vercel.app/',
+    gitHub: 'https://github.com/Asel85/neobis-front-giphyAPI',
+  },
+  {
+    title: 'Movie App',
+    img: project04,
+    skills: 'JavaScript,HTML, CSS, NPM, API',
+    href: 'https://cinema-five-phi.vercel.app/',
+    gitHub: 'https://github.com/Asel85/neobis-front-cinema',
+  },
+  {
+    title: 'Pokemon',
+    img: project05,
+    skills: 'JavaScript,HTML, CSS, NPM, API, React',
+    href: 'https://pokemon-project-wheat-sigma.vercel.app/',
+    gitHub: 'https://github.com/Asel85/pokemon',
+  },
+];
+export { projects };
