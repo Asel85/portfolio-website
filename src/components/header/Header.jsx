97a9a18a068596a2b5b24a5448f074ffe47c4234
Asel <aabdykalykova186@gmail.com>
2025-12-18 18:32:00 +0100

    CV added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import "./header.css";
+import React from 'react';
+import './header.css';
 
 const Header = () => {
   return (
     <header className="header">
-    <div className="header__wrapper">
+      <div className="header__wrapper">
         <h1 className="header__title">
-            <strong>Hi, my name is <em>Asel</em></strong><br />
-            a frontend developer
+          <strong>
+            Hi, my name is <em>Asel</em>
+          </strong>
+          <br />a frontend developer
         </h1>
         <div className="header__text">
-            <p>with passion for learning and creating.</p>
+          <p>with passion for learning and creating.</p>
         </div>
-        <a href="https://files.fm/u/dnbq74chrx#/view/drmc9e28j5" className="btn">Download CV</a>
-    </div>
-</header>
-  )
-}
+        <a
+          href="https://drive.google.com/file/d/1bpcEc6NBf4ahHA5OeCF6qC6PKSjfqEmH/view?usp=sharing"
+          className="btn">
+          Download CV
+        </a>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
